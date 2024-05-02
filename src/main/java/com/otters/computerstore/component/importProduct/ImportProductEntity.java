998cb1ed35181e3a1c10148e97074082b6a7c491
KailLow <21521551@gm.uit.edu.vn>
2024-05-02 09:07:31 +0700

    add Import Product Entity

---
+++ src/main/java/com/otters/computerstore/component/importProduct/ImportProductEntity.java
@@ -0,0 +1,38 @@
+package com.otters.computerstore.component.importProduct;
+
+import com.otters.computerstore.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class ImportProductEntity extends BaseEntity {
+    private String productId;
+    private Integer quantity;
+    private Integer index;
+    private Long price;
+    private String unit;
+
+    public ImportProductEntity(String productId, Integer quantity, Integer index, Long price, String unit) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.index = index;
+        this.price = price;
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "ImportProductEntity{" +
+                "productId='" + productId + '\'' +
+                ", quantity=" + quantity +
+                ", index=" + index +
+                ", price=" + price +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
+}
